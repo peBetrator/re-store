903fb551a38317eac750ec38945222d05ceb54e6
peBetrator <fivalit@gmail.com>
2019-07-22 22:39:53 +0300

    project structure

---
+++ src/components/app/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { withBookstoreService } from "../hoc";
+
+const App = ({ bookstoreService }) => {
+  console.log(bookstoreService.getBooks());
+  return <div>App</div>;
+};
+
+export default withBookstoreService()(App);
---
+++ src/services/bookstore-service.js
@@ -0,0 +1,8 @@
+export default class BookstoreService {
+  getBooks() {
+    return [
+      { id: 1, title: "First Book", author: "Author 1" },
+      { id: 2, title: "Second Book", author: "Author 2" }
+    ];
+  }
+}
