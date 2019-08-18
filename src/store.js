0c25e091ec487e274d6181c52d9456bd2514876b
peBetrator <fivalit@gmail.com>
2019-08-18 12:29:18 +0300

    middleware and thunk

--- src/store.js
+++ src/store.js
@@ -1,7 +1,26 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
 
 import reducer from "./reducers";
 
-const store = createStore(reducer);
+const logMiddleware = store => dispatch => action => {
+  console.log(action.type, store.getState());
+  return dispatch(action);
+};
+
+const stringMiddleware = () => dispatch => action => {
+  if (typeof action === "string") {
+    return dispatch({
+      type: action
+    });
+  }
+
+  return dispatch(action);
+};
+
+const store = createStore(
+  reducer,
+  applyMiddleware(thunkMiddleware, stringMiddleware, logMiddleware)
+);
 
 export default store;
